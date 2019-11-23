0a95b2d49b0ea4ae053a00111efbd5115b7faf35
Baklytsky <baklytsky@gmail.com>
2019-11-23 14:54:51 +0200

    section scroll ready

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,25 @@
             gutter: '.gutter-sizer',
             percentPosition: true
         });
+
+        $('#carouselExample').carousel({
+            interval :2000
+        })
+
+        $('#carouselExample .carousel-item').each(function(){
+            var next = $(this).next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+            next.children(':first-child').clone().appendTo($(this));
+
+            for (var i=0;i<8;i++) {
+                next=next.next();
+                if (!next.length) {
+                    next = $(this).siblings(':first');
+                }
+
+                next.children(':first-child').clone().appendTo($(this));
+            }
+        });
+
